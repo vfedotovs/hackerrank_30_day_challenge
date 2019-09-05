55466b03ce1a0f59f96dc431b1d1e7f39b5cf7b4
EC2 Default User <ec2-user@ip-172-31-14-85.eu-west-1.compute.internal>
2019-09-05 11:04:46 +0000

    Second  commmit adding app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+def main():
+    
+    hello()
+
+def hello():
+    name = input("What is ypur name: ")
+    
+    print("Hello ", name.capitalize())
+    print("This is hello world from python!!" )
+    
+
+if __name__ == "__main__":
+    main()
